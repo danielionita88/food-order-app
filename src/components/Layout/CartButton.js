b15b14f968531cd18418b82e20dc7c696a5b3e8c
Daniel Ionita <danielionita88@yahoo.com>
2022-07-29 12:24:27 -0500

    logic to remove items from cart + added animation to cart button

--- src/components/Layout/CartButton.js
+++ src/components/Layout/CartButton.js
@@ -1,18 +1,35 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 
 import CartContext from "../../store/cart-context";
 import CartIcon from "./CartIcon";
 import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
 
-  const numberOfCartItems = cartCtx.items.reduce((currentItem, item) => {
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+  const numberOfCartItems = items.reduce((currentItem, item) => {
     return currentItem + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
