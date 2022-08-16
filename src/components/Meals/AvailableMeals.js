653199df3f4d6e3e17bce9aa601e096b3c1c575e
Daniel Ionita <danielionita88@yahoo.com>
2022-08-16 15:28:50 -0500

    handle http errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,12 +5,18 @@ import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://food-order-app-170ae-default-rtdb.firebaseio.com/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!!!");
+      }
       const data = await response.json();
 
       const loadedMeals = [];
@@ -24,19 +30,35 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch(error => {
+      setIsLoading(false)
+      setHttpError(error.message)
+    })
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if(httpError){
+    return <section className={classes.MealsError}>
+      <p>{httpError}</p>
+    </section>
+  }
+
+  const mealsList = meals.map((meal) => <MealItem key={meal.id} meal={meal} />);
 
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>
-          {meals.map((meal) => (
-            <MealItem key={meal.id} meal={meal} />
-          ))}
-        </ul>
+        <ul>{mealsList}</ul>
       </Card>
     </section>
   );
